c3d5b633aa63fcc2871de0f89807188fd26b9419
neko3141592 <yudai3.1415926@gmail.com>
2025-11-28 20:36:37 +0900

    next

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"gin-quickstart/utils"
 	"gin-quickstart/db"
 	"gin-quickstart/handlers"
-	"github.com/gin-gonic/gin"
-	"github.com/deckarep/golang-set/v2"
+	"gin-quickstart/utils"
+
+	mapset "github.com/deckarep/golang-set/v2"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 const (
@@ -15,35 +16,45 @@ const (
 )
 
 var (
-	Graph map[int][]int
+	Graph     map[int][]int
 	sexualIDs mapset.Set[int]
 	idToTitle map[int]string
 	titleToID map[string]int
 )
 
-func main () {
+func main() {
 	err := db.ConnectDB()
 	if err != nil {
 		panic(err)
 	}
 
-	idToTitle, titleToID, sexualIDs, err := utils.BuildMap(ARTICLE_FILE, MAX_ARTICLES, db.DB, false)
-    if err != nil {
-        panic(err)
-    }
-
-	Graph, err := utils.BuildGraph(ARTICLE_FILE, MAX_ARTICLES, titleToID, db.DB, false)
-	if err != nil {
-		panic(err)
-	}
-
-	handlers.Graph = Graph
-    handlers.SexualIDs = sexualIDs
-    handlers.IdToTitle = idToTitle
-    handlers.TitleToID = titleToID
-    
-    r := gin.Default()
+	r := gin.Default()
 	r.Use(cors.Default())
-    r.GET("/api/path", handlers.FindShortestPath)
-    r.Run(":8080") 
+	r.GET("/api/articles", handlers.SearchArticles)
+
+
+	go func() {
+		idToTitle, titleToID, sexualIDs, err := utils.BuildMap(ARTICLE_FILE, MAX_ARTICLES, db.DB, false)
+		if err != nil {
+			panic(err)
+		}
+		Graph, err := utils.BuildGraph(ARTICLE_FILE, MAX_ARTICLES, titleToID, db.DB, false)
+		if err != nil {
+			panic(err)
+		}
+		handlers.Graph = Graph
+		handlers.SexualIDs = sexualIDs
+		handlers.IdToTitle = idToTitle
+		handlers.TitleToID = titleToID
+	}()
+
+	r.GET("/api/path", func(c *gin.Context) {
+		if handlers.Graph == nil {
+			c.JSON(503, gin.H{"error": "グラフ構築中です。しばらくお待ちください。"})
+			return
+		}
+		handlers.FindShortestPath(c)
+	})
+
+	r.Run(":8080")
 }
